ff66deee398f275cf873ef7815bba1e11bdb2cba
Javilonte <javgofi@gmail.com>
2024-06-04 19:20:10 -0600

    Trying to fix the icon

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,13 +1,43 @@
-import { View, Text } from 'react-native'
+import { Image, View, Text } from 'react-native'
 import React from 'react'
 import {Tabs, Redirect} from 'expo-router'
+import { icons } from '../../constants/icons'
 
+
+const TabIcon = (icon, color, name, focused) =>{
+  return (
+    <View>
+      <Image 
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text className={`${focused ? 'font-psemibold' : 'font-pregular'}`}>
+
+        {name}
+      </Text>
+    </View>
+  )
+}
 const TabsLayout = () => {
   return (
     <>
     <Tabs>
       <Tabs.Screen
         name="home"
+        options={{
+          title: 'Home',
+          headerShown: false, 
+          tabBarIcon:({color, focused}) => (
+            <TabIcon 
+              icon={icons.home}
+              color={color}
+              name="Home"
+              focused={focused}
+            />
+          )
+        }}
       />
     </Tabs>
     </>
---
+++ constants/icons.js
@@ -0,0 +1,5 @@
+
+export const icons = {
+    home: require('../assets/icons/home.png'),
+    // añade otros iconos aquí
+};

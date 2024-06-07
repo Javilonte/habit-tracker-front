eeabf6f1e8aab1d6046a1366730b6407188d1f8b
Javilonte <javgofi@gmail.com>
2024-06-06 19:07:50 -0600

    Created Bookmark, Create, Profile bottom menu icons

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -44,6 +44,51 @@ const TabsLayout = () => {
           )
         }}
       />
+      <Tabs.Screen
+        name="bookmark"
+        options={{
+          title: 'Bookmark',
+          headerShown: false, 
+          tabBarIcon:({color, focused}) => (
+            <TabIcon 
+              icon={icons.bookmark}
+              color={color}
+              name="Bookmark"
+              focused={focused}
+            />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: 'Create',
+          headerShown: false, 
+          tabBarIcon:({color, focused}) => (
+            <TabIcon 
+              icon={icons.create}
+              color={color}
+              name="Create"
+              focused={focused}
+            />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          headerShown: false, 
+          tabBarIcon:({color, focused}) => (
+            <TabIcon 
+              icon={icons.profile}
+              color={color}
+              name="Profile"
+              focused={focused}
+            />
+          )
+        }}
+      />
     </Tabs>
     </>
   )

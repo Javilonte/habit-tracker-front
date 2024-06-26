03fa41609f6d6cc0094fb2d2defda12c09a08dd5
Javilonte <javgofi@gmail.com>
2024-06-25 19:27:03 -0600

    Sign up page created

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,15 +1,78 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { useState } from "react";
+import { Link, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, ScrollView, Dimensions, Alert, Image } from "react-native";
 
-const SignUp
- = () => {
+import { images } from "../../constants";
+import { CustomButton, FormField } from "../../components";
+const SignUp = () => {
+
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
   return (
-    <View>
-      <Text>SignUp
-        
-      </Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View
+          className="w-full flex justify-center h-full px-4 my-6"
+          style={{
+            minHeight: Dimensions.get("window").height - 100,
+          }}
+        >
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[34px]"
+          />
 
-export default SignUp
+          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
+            Sign Up to HabitZen
+          </Text>
+
+          <FormField
+            title="Username"
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStyles="mt-10"
+          />
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Sign Up"
+            containerStyles="mt-7"
+          />
+
+          <View className="flex justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Have an account already?
+            </Text>
+            <Link
+              href="/sign-in"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Login
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
